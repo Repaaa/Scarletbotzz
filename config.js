62e6faf1ea869142d28a4c774ab63697e381da4a
repa-chan <108630477+repa-chan@users.noreply.github.com>
2022-07-04 09:09:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6285870305499'] //626285870305499
+global.ownername = "Repaa-chan"
+global.ytname = "YT: Xeon" /-
+global.socialm = "GitHub: DGXeon" //-
+global.location = "India, Mizoram, Aizawl" //Indonesia
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "Scarlet bot" //Scarlet bot
+global.websitex = "https://youtu.be/xn9RatOrbuI" //-
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "Scarlet Bot Stickers\n\n\n\n\n\n\n "
+global.author = "Repaa"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.ownertag = ['6285870305499'] //Repaa
+global.ownernummenu = ['6285870305499'] /6285870305499
+global.watermark = "Scarlet Bot Inc." //Scarlet bot Inc
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.linkz1 = "-"
+global.linkz2 = "-"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -92,7 +92,7 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['🗿']
 global.mess = {
     success: 'Done✓',
     admin: 'This feature is only for admins!',
