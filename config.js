da13e143259551d1bea3887f53a2d32c51b64eb1
repa-chan <108630477+repa-chan@users.noreply.github.com>
2022-07-04 09:19:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,23 +41,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['6285870305499'] //6285870305499
+global.ownername = "Repaa-chan"
+global.ytname = "YT: Xeon" //-
+global.socialm = "GitHub: DGXeon" //GitHub: Repaa-chan
+global.location = "India, Mizoram, Aizawl" //Indonesia
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
-global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.botname = "Scarlet bot" //Scarlet bot
+global.websitex = "https://youtu.be/xn9RatOrbuI" //unknown
+global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //-
+global.packname = "Scarlet Bot Stickers\n\n\n\n\n\n\n "
+global.author = Repaa-chan"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.ownertag = ['6285870305499'] //6285870305499
+global.ownernummenu = ['6285870305499'] //6285870305499
+global.watermark = "Xeon Bot Inc." //Scarlet bot Inc
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
 global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
 global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
@@ -92,17 +92,17 @@ global.ntilink = []
 global.lolhuman = "KaysaS"
 global.sessionName = "session"
 global.antitags = true
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['🗿']
 global.mess = {
     success: 'Done✓',
-    admin: 'This feature is only for admins!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This feature is only for owner',
-    group: 'This feature is only for groups!',
-    private: 'This feature is only for private chats!',
-    bot: 'This feature is only for bot',
+    admin: 'Fitur ini hanya untuk admin!',
+    botAdmin: 'Bot Harus Admin Dulu!',
+    owner: 'Fitur ini hanya untuk pemilik',
+    group: 'Fitur ini hanya untuk grup!',
+    private: 'Fitur ini hanya untuk obrolan pribadi!',
+    bot: 'Fitur ini hanya untuk bot',
     wait: 'In process...',
-    linkm: 'Where is the link?',
+    linkm: 'Mana linknya?',
     error: 'Error!',
     ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
